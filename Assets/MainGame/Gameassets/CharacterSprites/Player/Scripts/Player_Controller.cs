3fc15c3841b2f68ea78f1881051b1a225c991808
kallen19 <kaleighea8@gmail.com>
2026-07-13 09:35:18 -0400

    left and right Anim DONE!!!

--- Assets/MainGame/Gameassets/CharacterSprites/Player/Scripts/Player_Controller.cs
+++ Assets/MainGame/Gameassets/CharacterSprites/Player/Scripts/Player_Controller.cs
@@ -1,25 +1,40 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 [SelectionBase]
 public class Player_Controller : MonoBehaviour
 {
+#region 
+
+private enum Directions {UP, DOWN, LEFT, RIGHT}
+
+#endregion
 
 #region Editor Data
 [Header("Movement Attributes")]
 [SerializeField] float _moveSpeed = 50f;
  [Header("Dependencies")]
  [SerializeField] Rigidbody2D _rb;
+
+[SerializeField] Animator _animator;
+[SerializeField] SpriteRenderer _spriteRenderer;
 #endregion
 
 #region Internal Data
 private Vector2 _moveDir = Vector2.zero;
+private Directions _facingDirection = Directions.RIGHT;
+private readonly int _animMoveRight = Animator.StringToHash("Walking_Right");
+private readonly int _animIdeleRight = Animator.StringToHash("Idle_eight");
+
     #endregion
 
     #region Tick
     private void Update()
     {
-       GatherInput(); 
+       GatherInput();
+       CalculateFacingDirection();
+       UpdateAnimation(); 
     }
 
     private void FixedUpdate() //Fixed means anything with the physical system
@@ -34,15 +49,54 @@ private Vector2 _moveDir = Vector2.zero;
     {
         _moveDir.x = Input.GetAxisRaw("Horizontal");
         _moveDir.y = Input.GetAxisRaw("Vertical");
-
-        print(_moveDir); 
+ 
     }
     #endregion
 
     #region Movement Logic
     private void MovementUpdate()
     {
-        _rb.velocity = _moveDir * _moveSpeed * Time.fixedDeltaTime;
+        _rb.velocity = _moveDir.normalized * _moveSpeed * Time.fixedDeltaTime; //normalized helps to stop the player from moving faster in the diagonial direction
+    }
+    #endregion
+
+    #region Animation Logic
+    private void CalculateFacingDirection()
+    {
+        if(_moveDir.x != 0)
+        {
+            if (_moveDir.x > 0) //Moving Right
+            {
+                _facingDirection = Directions.RIGHT;
+            }
+            else if (_moveDir.x < 0) //Moving Left
+            {
+                _facingDirection = Directions.LEFT;
+            }
+
+        }
+    
+    }
+
+    private void UpdateAnimation()
+    {
+        if(_facingDirection == Directions.LEFT)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else if(_facingDirection == Directions.RIGHT)
+        {
+            _spriteRenderer.flipX = false;
+        }
+
+        if(_moveDir.SqrMagnitude() > 0) //We're moving
+        {
+            _animator.CrossFade(_animMoveRight, 0);
+        }
+        else
+        {
+            _animator.CrossFade(_animIdeleRight, 0);
+        }
     }
     #endregion
 } 
