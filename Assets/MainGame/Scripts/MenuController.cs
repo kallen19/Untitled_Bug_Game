70d71d1a0d6427f11063b1f685c05115cde78ede
kallen19 <kaleighea8@gmail.com>
2026-09-08 20:10:51 -0400

    Coding stuff

---
+++ Assets/MainGame/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menuCanvas;
+
+    
+    void Start()
+    {
+        menuCanvas.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+        }
+    }
+}
